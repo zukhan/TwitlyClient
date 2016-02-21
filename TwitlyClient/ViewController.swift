56a4cd8a9073aa8c52b7501d26f32431f5f2da6f
Zubair Khan <zkhan@box.com>
2016-02-21 01:16:52 -0800

    Initial Commit

---
+++ TwitlyClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitlyClient
+//
+//  Created by Zubair Khan on 2/21/16.
+//  Copyright © 2016 zapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
